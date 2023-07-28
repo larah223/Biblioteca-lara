68ff623e782fe73ba3f732f77246d9228ea231f4
LARA EVELYN MENEZES RAMOS <lara.emramos@senacsp.edu.br>
2023-07-28 17:25:22 -0300

    Adicionado o Rf e criado os models de Author e publisher

---
+++ Biblioteca/Data/BibliotecaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options) 
+        {
+
+        }
+    }
+}
