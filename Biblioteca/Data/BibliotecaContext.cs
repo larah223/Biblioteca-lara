d7030fb95ffa8762ba67708b47bdbadccbdd0627
LARA EVELYN MENEZES RAMOS <lara.emramos@senacsp.edu.br>
2023-07-31 17:28:47 -0300

    salvar

--- Biblioteca/Data/BibliotecaContext.cs
+++ Biblioteca/Data/BibliotecaContext.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Model;
 
 namespace Biblioteca.Data
 {
     public class BibliotecaContext : DbContext
     {
-        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options) 
+        public BibliotecaContext (DbContextOptions<BibliotecaContext> options)
+            : base(options)
         {
-
         }
+
+        public DbSet<Book> Book { get; set; } = default!;
+        public DbSet<Book> Publisher { get; set; } = default!;
+        public DbSet<Author> Author { get; set; } = default!;
     }
 }
